72a11a2e18d948963ca9c43442313063e9d5404d
Anderson Lozano <adslozanov@gmail.com>
2025-01-16 12:51:57 -0500

    :lipstick: Menu navigation is added and routes are configurate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { NotContentPageComponent } from './pages/not-content-page/not-content-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'about/history', component: NotContentPageComponent },
+  { path: '**', component: ErrorPageComponent }
+];
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [MatMenuModule, MatIconModule, CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  
+}
