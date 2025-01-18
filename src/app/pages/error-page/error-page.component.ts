6d6f49bc108ad91a273ebde371ec5274b5995296
Anderson Lozano <adslozanov@gmail.com>
2025-01-18 14:48:07 -0500

    :wrench: The metadata of the error pages are added.

--- src/app/pages/error-page/error-page.component.ts
+++ src/app/pages/error-page/error-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonReturnComponent } from "../../components/button-return/button-return.component";
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-error-page',
@@ -7,4 +8,19 @@ import { ButtonReturnComponent } from "../../components/button-return/button-ret
   styleUrls: ['./error-page.component.scss'],
   imports: [ButtonReturnComponent]
 })
-export class ErrorPageComponent {}
+export class ErrorPageComponent implements OnInit {
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+  ) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Página no encontrada - CD Futuro 13');
+    this.metaService.addTags([
+      { name: 'description', content: 'La página que buscas no se encuentra disponible en CD Futuro 13.' },
+      { name: 'keywords', content: 'CD Futuro 13, página no encontrada, error 404' },
+      { name: 'author', content: 'CD Futuro 13' },
+      { name: 'robots', content: 'noindex, nofollow' }
+    ]);
+  }
+}
--- src/app/pages/not-content-page/not-content-page.component.ts
+++ src/app/pages/not-content-page/not-content-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonReturnComponent } from "../../components/button-return/button-return.component";
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-content-page',
@@ -7,4 +8,19 @@ import { ButtonReturnComponent } from "../../components/button-return/button-ret
   styleUrls: [ './not-content-page.component.scss' ],
   imports: [ButtonReturnComponent]
 })
-export class NotContentPageComponent { }
+export class NotContentPageComponent implements OnInit {
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+  ) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Contenido no disponible - CD Futuro 13');
+    this.metaService.addTags([
+      { name: 'description', content: 'El contenido que buscas no está disponible en CD Futuro 13.' },
+      { name: 'keywords', content: 'CD Futuro 13, contenido no disponible, error' },
+      { name: 'author', content: 'CD Futuro 13' },
+      { name: 'robots', content: 'noindex, nofollow' }
+    ]);
+  }
+}
