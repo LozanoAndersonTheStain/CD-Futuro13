d7ba41fb608f7b51964713317923a0baabdb4131
Anderson Lozano <adslozanov@gmail.com>
2025-01-18 14:29:32 -0500

    :lipstick: added back button for error pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ButtonReturnComponent } from './components/button-return/button-return.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    ButtonReturnComponent
+  ]
 };
