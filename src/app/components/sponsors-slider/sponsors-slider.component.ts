1971944e34a0d51bd381959cd17ad85913ee1e17
Anderson Lozano <adslozanov@gmail.com>
2025-01-19 22:47:54 -0500

    :lipstick: Responsive design and logo images added

--- src/app/components/sponsors-slider/sponsors-slider.component.ts
+++ src/app/components/sponsors-slider/sponsors-slider.component.ts
@@ -10,11 +10,16 @@ import { CommonModule } from '@angular/common';
 })
 export class SponsorsSliderComponent implements OnInit {
   sponsors = [
-    { name: 'Sponsor 1', image: 'assets/sponsors/sponsor1.png' },
-    { name: 'Sponsor 2', image: 'assets/sponsors/sponsor2.png' },
-    { name: 'Sponsor 3', image: 'assets/sponsors/sponsor3.png' },
-    { name: 'Sponsor 4', image: 'assets/sponsors/sponsor4.png' },
-    { name: 'Sponsor 5', image: 'assets/sponsors/sponsor5.png' }
+    { name: 'Sponsor 1', image: 'assets/logos_sponsors/sponsor1.svg' },
+    { name: 'Sponsor 2', image: 'assets/logos_sponsors/sponsor2.svg' },
+    { name: 'Sponsor 3', image: 'assets/logos_sponsors/sponsor3.svg' },
+    { name: 'Sponsor 4', image: 'assets/logos_sponsors/sponsor4.svg' },
+    { name: 'Sponsor 5', image: 'assets/logos_sponsors/sponsor5.svg' },
+    { name: 'Sponsor 6', image: 'assets/logos_sponsors/sponsor6.svg' },
+    { name: 'Sponsor 7', image: 'assets/logos_sponsors/sponsor7.svg' },
+    { name: 'Sponsor 8', image: 'assets/logos_sponsors/sponsor8.svg' },
+    { name: 'Sponsor 9', image: 'assets/logos_sponsors/sponsor9.svg' },
+    { name: 'Sponsor 10', image: 'assets/logos_sponsors/sponsor10.svg' }
   ];
 
   constructor() {}
