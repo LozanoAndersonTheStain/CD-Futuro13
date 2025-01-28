08974d38739d493620b244948ddbd1cb1c540f2c
Anderson Lozano <adslozanov@gmail.com>
2025-01-28 14:57:41 -0500

    :wrench: The metadata of the Join-us page has been modified.

--- src/app/pages/join-us/join-us.component.ts
+++ src/app/pages/join-us/join-us.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-join-us',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './join-us.component.html',
   styleUrl: './join-us.component.scss'
 })
-export class JoinUsComponent {
+export class JoinUsComponent implements OnInit {
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+  ) {}
 
+  ngOnInit(): void {
+    this.titleService.setTitle('CD Futuro 13 - Únete');
+    this.metaService.addTags([
+      {
+        name: 'description',
+        content:
+          'Unete a CD Futuro 13 para formar parte de un club en el cual no solo aprenderas deporte, sino que tambien te formaras como persona.',
+      },
+      {
+        name: 'keywords',
+        content: 'CD Futuro 13, deporte, formación, jóvenes, comuna 13',
+      },
+      { name: 'author', content: 'CD Futuro 13' },
+    ]);
+  }
 }
