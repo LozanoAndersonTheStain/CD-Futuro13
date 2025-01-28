8eefa9dea53f12bb2f0b480268e1e9d3f734fe66
Anderson Lozano <adslozanov@gmail.com>
2025-01-27 21:26:57 -0500

    :lipstick: The administrators page is created with responsive included

---
+++ src/app/pages/team/administrators/administrators.component.ts
@@ -0,0 +1,106 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../components/button/button.component';
+import { MatIconModule } from '@angular/material/icon';
+import { ButtonConfig } from '../../../interfaces/button.interface';
+
+interface Administrator {
+  name: string;
+  age: number;
+  description?: string;
+  position?: string;
+  imageUrl: string;
+}
+
+@Component({
+  selector: 'app-administrators',
+  imports: [CommonModule, ButtonComponent, MatIconModule],
+  templateUrl: './administrators.component.html',
+  styleUrl: './administrators.component.scss'
+})
+export class AdministratorsComponent {
+  administrators: Administrator[] = [
+    {
+      name: 'Carlos Espinoza',
+      age: 33,
+      description: '',
+      imageUrl: '/assets/img_carousel/image_1.jpg',
+      position: 'Director'
+    },
+    {
+      name: 'Camila Perez',
+      age: 35,
+      description: '',
+      imageUrl: '/assets/img_carousel/image_2.jpg',
+      position: 'Subdirector'
+    },
+    {
+      name: 'Jhonfer Rodriguez',
+      age: 40,
+      description: '',
+      imageUrl: '/assets/img_carousel/image_3.jpg',
+      position: 'Administrador'
+    },
+    {
+      name: 'Sofía Velez',
+      age: 30,
+      description: '',
+      imageUrl: '/assets/img_carousel/image_4.jpg',
+      position: 'Administrador'
+    }
+  ]
+
+  currentAdministratorIndex = 0;
+  currentCardIndex = 0;
+
+
+  get currentAdministrator(): Administrator {
+    return this.administrators[this.currentAdministratorIndex];
+  }
+
+  updateCurrentAdministrator(index: number): void {
+    this.currentAdministratorIndex = index;
+  }
+
+  buttonConfig: ButtonConfig = {
+      label: 'Ver info del Administrador',
+      action: () => {
+        this.openModal(this.currentAdministratorIndex);
+      },
+      type: 'button',
+      class: 'btn-primary',
+      fontSize: '1rem',
+    };
+
+    openModal(index: number): void {
+      this.updateCurrentAdministrator(index);
+      const modal = document.getElementById('modal');
+      if (modal) {
+        modal.style.display = 'block';
+        const modalTitle = document.getElementById('modalTitle');
+        const modalDescription = document.getElementById('modalDescription');
+        if (modalTitle) {
+          modalTitle.textContent = this.currentAdministrator.name;
+        }
+        if (modalDescription) {
+          modalDescription.textContent = `Edad: ${this.currentAdministrator.age}. Ocupación en el club: ${this.currentAdministrator.position}`;
+        }
+      }
+    }
+
+
+  closeModal(): void {
+    const modal = document.getElementById('modal');
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  }
+
+  nextCard(): void {
+    this.currentCardIndex = (this.currentCardIndex + 1) % this.administrators.length;
+  }
+
+  prevCard(): void {
+    this.currentCardIndex = (this.currentCardIndex - 1 + this.administrators.length) % this.administrators.length;
+  }
+}
