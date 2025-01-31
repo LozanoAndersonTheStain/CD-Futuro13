b8ba45ddcbc3fbb9e3eea642b9b9758c076d03e1
Anderson Lozano <adslozanov@gmail.com>
2025-01-30 20:47:36 -0500

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,7 @@
+
+export default {
+  basePath: 'https://LozanoAndersonTheStain.github.io/CD-Futuro13/',
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
---
+++ server/angular-app-manifest.mjs
@@ -0,0 +1,103 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: 'https://LozanoAndersonTheStain.github.io/CD-Futuro13/',
+  locale: undefined,
+  routes: [
+  {
+    "renderMode": 2,
+    "redirectTo": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/home",
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/home"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/join-us"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/about"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/about/history"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/about/mission"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/about/vision"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/team"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/team/teachers"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/team/administrators"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/testimonials"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/testimonials/testimonials-fathers"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/testimonials/testimonials-players"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/trophy_room"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/tournaments"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/corporation"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/contact"
+  },
+  {
+    "renderMode": 2,
+    "route": "/https://LozanoAndersonTheStain.github.io/CD-Futuro13/**"
+  }
+],
+  assets: {
+    'index.csr.html': {size: 24186, hash: '77af6b06d6b36e3b530ec6eb95228740c62019ce171e4a41c3c2bcfb7a6aad9c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17254, hash: '621964079cae75d167c552a64e4453e9abddfc459281297e74063bfdb053cec3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'join-us/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/join-us_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'about/history/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/about_history_index_html.mjs').then(m => m.default)},
+    'about/mission/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/about_mission_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'about/vision/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/about_vision_index_html.mjs').then(m => m.default)},
+    'team/teachers/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/team_teachers_index_html.mjs').then(m => m.default)},
+    'testimonials/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/testimonials_index_html.mjs').then(m => m.default)},
+    'team/administrators/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/team_administrators_index_html.mjs').then(m => m.default)},
+    'testimonials/testimonials-fathers/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/testimonials_testimonials-fathers_index_html.mjs').then(m => m.default)},
+    'testimonials/testimonials-players/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/testimonials_testimonials-players_index_html.mjs').then(m => m.default)},
+    'tournaments/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/tournaments_index_html.mjs').then(m => m.default)},
+    'trophy_room/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/trophy_room_index_html.mjs').then(m => m.default)},
+    'corporation/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/corporation_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'team/index.html': {size: 53233, hash: '86cab3b490128eb116d81a1e5d08eafe945a363349f377af9618bf1ed9cb47e9', text: () => import('./assets-chunks/team_index_html.mjs').then(m => m.default)},
+    'styles-STFSWUPW.css': {size: 7494, hash: 'uSgDmy7CmmI', text: () => import('./assets-chunks/styles-STFSWUPW_css.mjs').then(m => m.default)}
+  },
+};
