a1cb73930b832282a9de0477f49fdf82efa78eb1
Anderson Lozano <adslozanov@gmail.com>
2025-02-14 22:22:39 -0500

    :lipstick: Tournament view is modified and tournament details page/view is added.

--- src/app/pages/tournaments/tournament-details/tournament-details.component.ts
+++ src/app/pages/tournaments/tournament-details/tournament-details.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { TournamentService } from '../../../services/tournament.service';
+import { Subscription } from 'rxjs';
+import { ButtonComponent } from "../../../components/button/button.component";
+import { ButtonConfig } from '../../../interfaces/button.interface';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-tournament-details',
-  imports: [],
+  imports: [CommonModule, ButtonComponent, RouterModule, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './tournament-details.component.html',
-  styleUrl: './tournament-details.component.scss'
+  styleUrls: ['./tournament-details.component.scss']
 })
-export class TournamentDetailsComponent {
+export class TournamentDetailsComponent implements OnInit, OnDestroy {
+  tournament: any;
+  matches: any[] = [];
+  isLoading = true;
+  private subscription: Subscription = new Subscription();
 
+  constructor(
+    private route: ActivatedRoute,
+    private tournamentService: TournamentService,
+    private router: Router
+  ) {}
+
+  buttonConfig: ButtonConfig = {
+    label: 'Volver a la lista de torneos',
+    action: () => {
+      this.navigateToTournaments();
+    },
+    type: 'button',
+    class: 'btn-primary',
+    fontSize: '1rem',
+    href: '/tournaments',
+    icon: 'arrow_back',
+    iconPosition: 'left'
+  };
+
+  navigateToTournaments(): void {
+    this.router.navigate(['/tournaments']);
+  }
+  ngOnInit(): void {
+    const tournamentId = this.route.snapshot.paramMap.get('id');
+    if (tournamentId) {
+      this.subscription.add(
+        this.tournamentService.getTournamentById(tournamentId).subscribe((data) => {
+          this.tournament = data;
+          this.matches = data?.matches ? Object.values(data.matches) : [];
+          this.isLoading = false;
+        })
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/pages/tournaments/tournaments.component.ts
+++ src/app/pages/tournaments/tournaments.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TournamentService } from '../../services/tournament.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-tournaments',
-  imports: [],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './tournaments.component.html',
-  styleUrl: './tournaments.component.scss'
+  styleUrls: ['./tournaments.component.scss']
 })
-export class TournamentsComponent {
+export class TournamentsComponent implements OnInit {
+  tournaments: any[] = [];
+  isLoading = true;
 
+  constructor(private router: Router, private tournamentService: TournamentService) {}
+
+  ngOnInit(): void {
+    this.tournamentService.getTournaments().subscribe((data) => {
+      this.tournaments = data;
+      this.isLoading = false;
+    });
+  }
+
+  navigateToTournament(tournament: any): void {
+    this.router.navigate(['/tournaments', tournament.id]);
+  }
 }
