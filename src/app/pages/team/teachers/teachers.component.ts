2ff1bebe134ee49be7f1556df9bc25d54a8a5c23
Anderson Lozano <adslozanov@gmail.com>
2025-03-03 22:43:12 -0500

    🔨 New information for teachers has been added, in addition to changing the way to access the images and modifying some of the styles for a better visualization.

--- src/app/pages/team/teachers/teachers.component.ts
+++ src/app/pages/team/teachers/teachers.component.ts
@@ -2,18 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ButtonComponent } from "../../../components/button/button.component";
 import { ButtonConfig } from '../../../interfaces/button.interface';
+import { Teachers } from '../../../interfaces/teachers.interface';
 import { MatIconModule } from '@angular/material/icon';
 import { Meta, Title } from '@angular/platform-browser';
-
-interface Teachers {
-  name: string;
-  age: number;
-  description?: string;
-  categories: string[];
-  imageUrl: string;
-  tournaments?: string[];
-  tournamentsWins?: string[];
-}
 
 @Component({
   selector: 'app-teachers',
@@ -22,7 +13,6 @@ interface Teachers {
   styleUrl: './teachers.component.scss'
 })
 export class TeachersComponent implements OnInit {
-
   constructor(
     private titleService: Title,
     private metaService: Meta,
@@ -30,58 +20,60 @@ export class TeachersComponent implements OnInit {
 
   teachers: Teachers[] = [
     {
-      name: 'Santos Eliecer',
-      age: 43,
-      description: '',
-      categories: ['Categoría 1', 'Categoría 2'],
-      imageUrl: 'assets/img_carousel/image_1.jpg',
-      tournaments: ['Tournament 1', 'Tournament 2'],
-      tournamentsWins: ['Tournament 1'],
+      name: 'Jhon Nelson Salazar Villa',
+      description: 'Especialista en categorías Sub4 a Sub7, Jhon Nelson enfoca su enseñanza en habilidades motoras, coordinación y diversión. Con metodología lúdica, sienta las bases futbolísticas y personales de los más pequeños, fomentando el amor por el deporte desde temprana edad.',
+      categories: ['Categoría Sub4', 'Categoría Sub5', 'Categoría Sub6', 'Categoría Sub7'],
+      imageUrl: ['https://res.cloudinary.com/dy6jglszo/image/upload/v1741056223/Jhon_Nelson_Salazar_Villa_2_bdfxia.jpg', 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741056223/Jhon_Nelson_Salazar_Villa_1_sl74je.jpg'],
+      tournaments: ['Sin Registro'],
+      tournamentsWins: ['Sin Registro'],
     },
     {
-      name: 'María Pérez',
-      age: 35,
-      description: '',
-      categories: ['Categoría 3', 'Categoría 4'],
-      imageUrl: 'assets/img_carousel/image_2.jpg',
-      tournaments: ['Tournament 3', 'Tournament 4'],
-      tournamentsWins: ['Tournament 3', 'Tournament 4'],
+      name: 'Juan Esteban Garcia Vanegas',
+      description: 'Trabaja con Sub8 y Sub9, perfeccionando técnicas básicas como control, pase y conducción. Su enfoque en valores como disciplina y trabajo en equipo lo convierte en un guía ideal para niños que inician su camino en el fútbol de manera más seria.',
+      categories: ['Categoría Sub8', 'Categoría Sub9'],
+      imageUrl: ['https://res.cloudinary.com/dy6jglszo/image/upload/v1741056223/Juan_Esteban_Garcia_Vanegas_2_bde2og.jpg', 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741056223/Juan_Esteban_Garcia_Vanegas_1_cykowl.jpg'],
+      tournaments: ['Sin Registro'],
+      tournamentsWins: ['Sin Registro'],
     },
     {
-      name: 'Juan López',
-      age: 40,
-      description: '',
-      categories: ['Categoría 5', 'Categoría 6'],
-      imageUrl: 'assets/img_carousel/image_3.jpg',
-      tournaments: ['Tournament 5', 'Tournament 6'],
-      tournamentsWins: ['Tournament 6'],
+      name: 'Cristino Álvarez Moreno',
+      description: 'Experto en Sub10 y Sub11, Cristino desarrolla habilidades técnicas y tácticas, enfocándose en la inteligencia deportiva y la toma de decisiones. Prepara a los jóvenes para desafíos más complejos, consolidando su crecimiento futbolístico con un enfoque integral.',
+      categories: ['Categoría Sub10', 'Categoría Sub11'],
+      imageUrl: ['https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Cristino_%C3%81lvarez_Moreno_1_stwdgz.jpg', 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Cristino_%C3%81lvarez_Moreno_2_xabvlw.jpg'],
+      tournaments: ['Sin Registro'],
+      tournamentsWins: ['Sin Registro'],
     },
     {
-      name: 'Ana García',
-      age: 30,
-      description: '',
-      categories: ['Categoría 7', 'Categoría 8'],
-      imageUrl: 'assets/img_carousel/image_4.jpg',
-      tournaments: ['Tournament 7', 'Tournament 8'],
-      tournamentsWins: ['Aun no ha ganado torneos'],
+      name: 'Juan Esteban Perea Andrade',
+      description: 'Especializado en Sub12, fortalece las habilidades físicas y técnicas de los jugadores. Su enfoque en disciplina y esfuerzo constante prepara a los jóvenes para la transición a categorías más avanzadas, ayudándoles a alcanzar su máximo potencial.',
+      categories: ['Categoría Sub12'],
+      imageUrl: ['https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Imagen_de_WhatsApp_2025-03-03_a_las_19.30.57_f49fea6b_ys8qhv.jpg', 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Imagen_de_WhatsApp_2025-03-03_a_las_19.30.57_23cd5ee4_nikdql.jpg'],
+      tournaments: ['Sin Registro'],
+      tournamentsWins: ['Sin Registro'],
     },
     {
-      name: 'Carlos Martínez',
-      age: 45,
-      description: '',
-      categories: ['Categoría 9', 'Categoría 10'],
-      imageUrl: 'assets/img_carousel/image_5.jpg',
-      tournaments: ['Tournament 9', 'Tournament 10'],
-      tournamentsWins: ['Aun no ha ganado torneos'],
+      name: 'Carlos Javier Aguilar Ruiz',
+      description: 'Trabaja con Sub13 y Sub14, combinando técnica, táctica y preparación física. Su metodología incluye mentalidad competitiva, preparando a los jóvenes para torneos y desafíos con confianza y determinación, impulsando su desarrollo integral.',
+      categories: ['Categoría Sub13', 'Categoría Sub14'],
+      imageUrl: ['https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Imagen_de_WhatsApp_2025-03-03_a_las_19.33.59_57aa5911_cr7b2r.jpg', 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Imagen_de_WhatsApp_2025-03-03_a_las_19.33.59_4ebe8cbc_tvqmxj.jpg'],
+      tournaments: ['Sin Registro'],
+      tournamentsWins: ['Sin Registro'],
     },
     {
-      name: 'Laura Rodríguez',
-      age: 38,
-      description: '',
-      categories: ['Categoría 11', 'Categoría 12'],
-      imageUrl: 'assets/img_carousel/image_6.jpg',
-      tournaments: ['Tournament 11', 'Tournament 12'],
-      tournamentsWins: ['Tournament 12'],
+      name: 'Ferley Palacios Palacios',
+      description: 'Especialista en Sub15 a Sub17, Ferley enfoca su enseñanza en táctica avanzada, preparación física y mentalidad ganadora. Es clave para jóvenes que buscan dar el salto al fútbol profesional, puliendo su talento y preparándolos para el alto rendimiento.',
+      categories: ['Categoría Sub15', 'Categoría Sub16', 'Categoría Sub17'],
+      imageUrl: ['https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Imagen_de_WhatsApp_2025-03-03_a_las_19.35.34_4499c2eb_tsmjsa.jpg', 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741056222/Imagen_de_WhatsApp_2025-03-03_a_las_19.35.34_05c6eefc_dy6sij.jpg'],
+      tournaments: ['Sin Registro'],
+      tournamentsWins: ['Sin Registro'],
+    },
+    {
+      name: 'Duban David Fuentes Barrios',
+      description: 'Entrenador de arqueros, Duban David desarrolla habilidades técnicas, posicionamiento y lectura del juego. Con un enfoque en la mentalidad fuerte bajo presión, forma porteros excepcionales, preparándolos para destacar en esta posición clave dentro del fútbol.',
+      categories: ['Arqueros'],
+      imageUrl: ['https://res.cloudinary.com/dy6jglszo/image/upload/v1741056223/Imagen_de_WhatsApp_2025-03-03_a_las_19.37.06_0c4199cc_djnrrw.jpg', 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741056223/Imagen_de_WhatsApp_2025-03-03_a_las_19.37.06_b0ef162d_k84nuh.jpg'],
+      tournaments: ['Sin Registro'],
+      tournamentsWins: ['Sin Registro'],
     }
   ];
 
@@ -89,7 +81,15 @@ export class TeachersComponent implements OnInit {
   currentCardIndex = 0;
 
   get currentTeacher(): Teachers {
-    return this.teachers[this.currentTeacherIndex]
+    return this.teachers[this.currentTeacherIndex];
+  }
+
+  get currentTeacherFirstImage(): string {
+    return this.teachers[this.currentTeacherIndex].imageUrl[0];
+  }
+
+  get currentTeacherSecondImage(): string {
+    return this.teachers[this.currentTeacherIndex].imageUrl[1];
   }
 
   updateCurrentTeacher(index: number): void {
