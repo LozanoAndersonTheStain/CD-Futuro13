b78aaa4bf7e33be914d2f9435a7200e051cc3fcf
Anderson Lozano <adslozanov@gmail.com>
2025-03-06 16:10:38 -0500

    📸🔨 New images are added for the carousel, and the logic for the modal functionality is added.

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -12,19 +12,28 @@ import { CarouselModule } from '@syncfusion/ej2-angular-navigations';
 export class CarouselComponent implements OnInit {
 
   public slidesData: { image: string, name: string }[] = [
-    { image: 'assets/img_carousel/image_1.jpg', name: 'Image 1' },
-    { image: 'assets/img_carousel/image_2.jpg', name: 'Image 2' },
-    { image: 'assets/img_carousel/image_3.jpg', name: 'Image 3' },
-    { image: 'assets/img_carousel/image_4.jpg', name: 'Image 4' },
-    { image: 'assets/img_carousel/image_5.jpg', name: 'Image 5' },
-    { image: 'assets/img_carousel/image_6.jpg', name: 'Image 6' },
-    { image: 'assets/img_carousel/image_7.jpg', name: 'Image 7' },
-    { image: 'assets/img_carousel/image_8.jpg', name: 'Image 8' },
-    { image: 'assets/img_carousel/image_9.jpg', name: 'Image 9' }
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290307/Futuro13/Carrousel-home/cap1.jpg', name: 'Image 1' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290306/Futuro13/Carrousel-home/cap2.jpg', name: 'Image 2' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290306/Futuro13/Carrousel-home/cap3.jpg', name: 'Image 3' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290306/Futuro13/Carrousel-home/cap4.jpg', name: 'Image 4' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290305/Futuro13/Carrousel-home/cap5.jpg', name: 'Image 5' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290305/Futuro13/Carrousel-home/cap6.jpg', name: 'Image 6' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290305/Futuro13/Carrousel-home/cap7.jpg', name: 'Image 7' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290304/Futuro13/Carrousel-home/cap8.jpg', name: 'Image 8' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290304/Futuro13/Carrousel-home/cap9.jpg', name: 'Image 9' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290304/Futuro13/Carrousel-home/cap10.jpg', name: 'Image 10' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290303/Futuro13/Carrousel-home/cap11.jpg', name: 'Image 11' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290303/Futuro13/Carrousel-home/cap12.jpg', name: 'Image 12' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290303/Futuro13/Carrousel-home/cap13.jpg', name: 'Image 13' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290302/Futuro13/Carrousel-home/cap14.jpg', name: 'Image 14' },
+    { image: 'https://res.cloudinary.com/dy6jglszo/image/upload/v1741290302/Futuro13/Carrousel-home/cap15.jpg', name: 'Image 15' },
   ];
   visibleSlides: { image: string, name: string }[] = [];
   currentIndex: number = 0;
   slidesToShow: number = 6;
+
+  isModalOpen = false;
+  selectedImage: { image: string, name: string } | null = null;
 
   ngOnInit(): void {
     this.updateVisibleSlides();
@@ -45,5 +54,15 @@ export class CarouselComponent implements OnInit {
   getNext(): void {
     this.currentIndex = (this.currentIndex + 1) % this.slidesData.length;
     this.updateVisibleSlides();
+  }
+
+  openModal(slide: { image: string, name: string }): void {
+    this.selectedImage = slide;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.selectedImage = null;
   }
 }
