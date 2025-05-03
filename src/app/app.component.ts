0dbdba86f83ac061e07e5b3bf3e52352f15cd2b1
Anderson Lozano <adslozanov@gmail.com>
2025-05-03 10:35:37 -0500

    🔍 The format of the structure is adjusted.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'CD-Futuro13';
 
-  constructor(
-    private titleService: Title,
-    private metaService: Meta,
-  ) {}
+  constructor(private titleService: Title, private metaService: Meta) {}
 
   ngOnInit(): void {
     this.titleService.setTitle('CD Futuro 13');
     this.metaService.addTags([
-      { name: 'description', content: 'CD Futuro 13 es un club deportivo comprometido con la formación de jóvenes íntegros.' },
-      { name: 'keywords', content: 'CD Futuro 13, deporte, formación, jóvenes, comuna 13' },
+      {
+        name: 'description',
+        content:
+          'CD Futuro 13 es un club deportivo comprometido con la formación de jóvenes íntegros.',
+      },
+      {
+        name: 'keywords',
+        content: 'CD Futuro 13, deporte, formación, jóvenes, comuna 13',
+      },
       { name: 'author', content: 'CD Futuro 13' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'robots', content: 'index, follow' }
+      { name: 'robots', content: 'index, follow' },
     ]);
   }
 }
-
-
