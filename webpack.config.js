f7147aebcbb13d878fa7dfe0e8b3e94d4e7d8ab4
Anderson Lozano <adslozanov@gmail.com>
2025-08-12 11:45:25 -0500

    ♻️ refactor(build): switch to angular-devkit builders and optimize webpack config
    Remove custom webpack configuration in favor of default angular-devkit builders
    Only load .env variables in development environment to improve production security

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
-// Cargar variables de entorno desde .env
-const env = dotenv.config().parsed;
+// Solo cargar .env para development
+if (process.env['NODE_ENV'] !== 'production') {
+  const env = dotenv.config().parsed || {};
 
-// Reducir las variables de entorno a un objeto para DefinePlugin
-const envKeys = Object.keys(env || {}).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
 
-module.exports = {
-  plugins: [
-    new webpack.DefinePlugin(envKeys)
-  ]
-};
+  module.exports = {
+    plugins: [
+      new webpack.DefinePlugin(envKeys)
+    ]
+  };
+} else {
+  module.exports = {};
+}
